8add7fc2c0746c4e98189c5081c4d701b458c8c4
Tejshree <tj.241094@gmail.com>
2019-04-10 00:04:29 -0700

    Sum of digits of number is Palindrome or not

--- Mathematical/Sum of digits of a number.py
+++ Mathematical/Sum of digits of a number.py
@@ -6,7 +6,7 @@ The first line of the input contains T denoting the number of testcases. T testc
 Then each of the T lines contains single positive integer N denoting the value of number.
 
 Output:
-For each testcase, in a new line, output "YES" if pallindrome else "NO". (without the quotes)
+For each testcase, in a new line, output "YES" if palindrome else "NO". (without the quotes)
 
 Constraints:
 1 <= T <= 200
@@ -23,28 +23,24 @@ NO  (9+8 = 17. Not a Palindrome)
 """
 
 
-# def sum_palindrome(N):
-#     N = (int(x) for x in str((int(input.split()))))
-#     if N[0] == N[1]:
-#         print("YES")
-#     else:
-#         print("NO")
-#
-#
-# for _ in range(int(input())):
-#     sum_palindrome(int(input()))
-#
-
-def sumOfDigits(N):
+def sum_of_digits(N):
     addition = 0
     while N:
         addition += N % 10
         N //= 10
-    return print(addition)
+    return addition
 
 
-def isPalindrome(addition):
+def isPalindrome(N):
+    temp = N
+    reverse = 0
+    while temp:
+        reverse = (reverse*10) + temp % 10
+        temp //= 10
+    return N == reverse
 
 
-
-sumOfDigits(int(input()))
+for _ in range(int(input())):
+    N = int(input())
+    N = sum_of_digits(N)
+    print("YES" if isPalindrome(N) else "NO")
