b932658998be9a523625e9fb5a22fefe046eaa93
Tejshree <tj.241094@gmail.com>
2019-04-25 23:38:20 -0700

    String sorting

---
+++ String/Sort the string.py
@@ -0,0 +1,23 @@
+"""
+Given a string S containing only lower case alphabets, the task is to sort it in lexigraphically-descending order.
+
+Input:
+The first line of input contains an integer T denoting the number of test cases. Then T test cases follow.
+Each test case contains a string S.\
+
+Output:
+For each test case, in a new line, print the sorted string.
+
+Constraints:
+1 <= T <= 100
+1 <= |S| <= 105
+
+Example:
+Input:
+2
+geeks
+for
+Output:
+skgee
+rof
+"""
