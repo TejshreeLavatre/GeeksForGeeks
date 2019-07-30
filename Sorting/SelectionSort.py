e460fecd227cd4cb46b2b2afbd7a32b4355d3654
Tejshree <tj.241094@gmail.com>
2019-07-30 00:00:46 -0700

    Selection sort

---
+++ Sorting/SelectionSort.py
@@ -0,0 +1,20 @@
+# time complexity is O(n^2)
+
+#
+# def selectionSort(A):
+#     for i in range(len(A)-1):
+#         minIndex = i
+#         for j in range(i+1, len(A)):
+#             if A[j] < A[minIndex]:
+#                 minIndex = j
+#         if minIndex != i:
+#             A[i], A[minIndex] = A[minIndex], A[i]
+#     return print(A)
+#
+#
+# selectionSort([4, 5, 6, 1, 2, 7, 8, 3])
+
+for _ in range(int(input())):
+    N = int(input())
+    A = input().split(" ")
+    print(A)
